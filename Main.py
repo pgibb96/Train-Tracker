06e2403a8970674132b0b335306a97e14e0d4909
pgibb96 <pgibb96@gmail.com>
2020-02-25 19:18:07 -0500

    Added the color change depending on urgency, and the minutes left before it leaves

--- Main.py
+++ Main.py
@@ -4,17 +4,20 @@
 import requests
 import time
 
+#Third Party
+from datetime import datetime, date
+
 #Local 
 from samplebase import SampleBase
 from rgbmatrix import graphics
 
 first = ""
 second = ""
+timeout = time.time() + 60
 
 class NextTrain(SampleBase):
     def __init__(self, *args, **kwargs):
         super(NextTrain, self).__init__(*args, **kwargs)
-        #self.parser.add_argument("-t", "--text", help="Next Train", default=str("Next Train at: "))
 
     def determineNext(self):
         URL = 'https://api-v3.mbta.com/schedules'
@@ -53,16 +56,38 @@ class NextTrain(SampleBase):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("6x10.bdf")
+
+        current = datetime.now().time()
+        first = datetime.strptime(self.determineNext()[0], '%H:%M').time()
+        second = datetime.strptime(self.determineNext()[1], '%H:%M').time()
+
+        firstLeft = datetime.combine(date.min, first) - datetime.combine(date.min, current)
+        secondLeft = datetime.combine(date.min, second) - datetime.combine(date.min, current)
+
+        firstLeft = int(round(firstLeft.seconds / 60))
+        secondLeft = int(round(secondLeft.seconds / 60))
+
         textColor = graphics.Color(255, 255, 0)
+
+        # Text Changes Color to Indicate Urgency (only can represent the next train)
+        if firstLeft > 7:
+            textColor = graphics.Color(0, 255, 0)
+        elif firstLeft > 5:
+            pass
+        else:
+            textColor = graphics.Color(255, 0, 0)
+
         pos = 0
-        my_text = "Next: " + self.determineNext()[0] + "    After: " + self.determineNext()[1]
-        while True:
+        my_text = "Next: " + self.determineNext()[0] + " (" + str(firstLeft) + "m)" + "      After: " + self.determineNext()[1] + " (" + str(secondLeft) + "m)"
+        
+        while time.time() < timeout:
             offscreen_canvas.Clear()
             len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
             pos -= 1
+            
             if (pos + len < 0):
                 pos = offscreen_canvas.width
-
+                
             time.sleep(0.05)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
